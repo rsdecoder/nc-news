f4f6ebf2eff725bbd59bc57ce0bab8ac2a1ab511
rsdecoder <raginisampath9@gmail.com>
2024-02-23 16:56:00 +0000

    Reverted back the changes

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,11 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE not set');
+}
+
 const config = {}
 
 if (ENV === 'production'){
@@ -12,9 +17,6 @@ if (ENV === 'production'){
   config.max = 2;
 }
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE not set');
-} 
 
 console.log(`The database is ${process.env.PGDATABASE}`);
 
