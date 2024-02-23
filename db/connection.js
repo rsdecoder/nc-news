52d75c155282f4229c3dea4480ff5f8494041216
rsdecoder <raginisampath9@gmail.com>
2024-02-23 15:49:15 +0000

    fixed a spelling mistake

--- db/connection.js
+++ db/connection.js
@@ -11,7 +11,7 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
 const config = {}
 
 if (ENV === 'production'){
-  config.connectString = process.env.DATABASE_URL;
+  config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
 } 
 
